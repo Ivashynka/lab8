740a455cac8e8bd221ce1e12b36ec7b06dd47228
EugeneThunder <58840394+EugeneThunder@users.noreply.github.com>
2019-12-13 10:58:52 +0300

    Add files via upload

--- lab8.dpr
+++ lab8.dpr
@@ -1,26 +1,17 @@
-﻿program lab8;
+program lab8;
 
 {$APPTYPE CONSOLE}
 
 {$R *.res}
+
 uses
   System.SysUtils;
-var I,O:textfile;
-s:string;
-begin
-assignFile(I,'C:\Users\ivash\Desktop\OAiP(LR)\lab8\input.txt');
-assignFile(O,'C:\Users\ivash\Desktop\OAiP(LR)\lab8\output.txt');
-reset(I);
-while not(EOF(I)) do
-  begin
-  readln(I,s);
-  writeln(s);
-  end;
-  closefile(I);
-  Rewrite(o) ;
 
-  append(o);
-  Writeln(o,'fjlkdj');
-  closefile(O) ;
-  readln;
+begin
+  try
+    { TODO -oUser -cConsole Main : Insert code here }
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
 end.
